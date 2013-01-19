a1ff661028cfe7339ba8f887546fc941ac3b7a78
Chris Carey <cpc2273@rit.edu>
2013-01-18 22:18:10 -0500

    Prevents site access from users not logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize, :except => :login
   protect_from_forgery
+
+protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      flash[:notice] = "Please log in"
+      redirect_to :controller => 'admin', :action => 'login'
+    end
+  end
+
 end
