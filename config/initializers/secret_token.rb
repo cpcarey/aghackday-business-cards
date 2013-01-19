baf85bda13157e9889ce964323ae8317caadb2dd
Chris Carey <cpc2273@rit.edu>
2013-01-18 20:39:55 -0500

    Initial commit (2)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AghackdayBusinessCards::Application.config.secret_token = 'af7d9dbf36e351456252076ea738f764f70451b9ffcb3396fd79db4cbbb780d3f8943f9176689a7d88b367c786de16c5d6952bf807965a58ea08fe11325635d8'
